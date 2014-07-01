7de435e89473e862a68d7c37d496344ca914a383
Olli Helenius <olli.helenius@onesto.fi>
2014-07-01 18:18:24 +0300

    Add discard helper for emptying an InputStream

--- src/main/scala/fi/onesto/sbt/mobilizer/util.scala
+++ src/main/scala/fi/onesto/sbt/mobilizer/util.scala
@@ -1,7 +1,11 @@
 package fi.onesto.sbt.mobilizer
 
+import java.io.InputStream
+
 
 object util {
+  private val DiscardBufferSize = 32768
+
   implicit class Tap[A](a: A) {
     def tap[B](action: A => B): A = {
       action(a)
@@ -21,7 +25,12 @@ object util {
   }
 
   def shellQuote(command: String, args: String*): String = shellQuote(command +: args)
-  def shellQuote(args: Seq[String]): String = args.map(shellQuote(_)).mkString(" ")
+  def shellQuote(args: Seq[String]): String = args.map(shellQuote).mkString(" ")
 
   val currentUser = Option(System.getProperty("user.name")).getOrElse("root")
+
+  def discard(stream: InputStream) {
+    val buffer = new Array[Byte](DiscardBufferSize)
+    while (stream.read(buffer) != -1) {}
+  }
 }
