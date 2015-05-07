d842279af324260209234aa44fbf8127c0f920ff
Olli Helenius <olli.helenius@onesto.fi>
2015-05-07 10:22:36 +0300

    Adjust all non-minimum levels down to DEBUG
    SSHJ logs all kinds of uninteresting stuff at INFO level, we'll filter them out from SBT console logging.

--- src/main/scala/fi/onesto/sbt/mobilizer/Slf4jSbtLogger.scala
+++ src/main/scala/fi/onesto/sbt/mobilizer/Slf4jSbtLogger.scala
@@ -14,11 +14,11 @@ final class Slf4jSbtLogger(
 
   private[this] final val serialVersionUID: Long = 1L
 
-  private[this] def atLevel(level: Level.Value): Boolean = {
+  private[this] def adjustLevel(level: Level.Value): Level.Value = {
     if (level.id >= minimumLevel.id) {
-      underlying.atLevel(level)
+      level
     } else {
-      false
+      Level.Debug
     }
   }
 
@@ -37,48 +37,48 @@ final class Slf4jSbtLogger(
     Option(result.getThrowable).map{t => s"${result.getMessage}: $t"}.getOrElse(result.getMessage)
   }
 
-  override def isTraceEnabled: Boolean                 = atLevel(Debug)
-  override def isTraceEnabled(marker: Marker): Boolean = atLevel(Debug)
+  override def isTraceEnabled: Boolean                 = underlying.atLevel(Debug)
+  override def isTraceEnabled(marker: Marker): Boolean = underlying.atLevel(Debug)
 
-  override def trace(msg: String): Unit                             = if (isTraceEnabled){underlying.debug(s"$name $msg")}
-  override def trace(msg: String, arg: AnyRef): Unit                = if (isTraceEnabled){underlying.debug(s"$name ${fmt(msg, arg)}")}
-  override def trace(msg: String, arg1: AnyRef, arg2: AnyRef): Unit = if (isTraceEnabled){underlying.debug(s"$name ${fmt(msg, arg1, arg2)}")}
-  override def trace(msg: String, arguments: AnyRef*): Unit         = if (isTraceEnabled){underlying.debug(s"$name ${fmt(msg, arguments)}")}
-  override def trace(msg: String, t: Throwable): Unit               = if (isTraceEnabled){underlying.debug(s"$name $msg: ${t.toString}")}
+  override def trace(msg: String): Unit                             = underlying.log(adjustLevel(Debug), s"$name $msg")
+  override def trace(msg: String, arg: AnyRef): Unit                = underlying.log(adjustLevel(Debug), s"$name ${fmt(msg, arg)}")
+  override def trace(msg: String, arg1: AnyRef, arg2: AnyRef): Unit = underlying.log(adjustLevel(Debug), s"$name ${fmt(msg, arg1, arg2)}")
+  override def trace(msg: String, arguments: AnyRef*): Unit         = underlying.log(adjustLevel(Debug), s"$name ${fmt(msg, arguments)}")
+  override def trace(msg: String, t: Throwable): Unit               = underlying.log(adjustLevel(Debug), s"$name $msg: ${t.toString}")
 
-  override def isDebugEnabled: Boolean                 = atLevel(Debug)
-  override def isDebugEnabled(marker: Marker): Boolean = atLevel(Debug)
+  override def isDebugEnabled: Boolean                 = underlying.atLevel(Debug)
+  override def isDebugEnabled(marker: Marker): Boolean = underlying.atLevel(Debug)
 
-  override def debug(msg: String): Unit                             = if(isDebugEnabled){underlying.debug(s"$name $msg")}
-  override def debug(msg: String, arg: AnyRef): Unit                = if(isDebugEnabled){underlying.debug(s"$name ${fmt(msg, arg)}")}
-  override def debug(msg: String, arg1: AnyRef, arg2: AnyRef): Unit = if(isDebugEnabled){underlying.debug(s"$name ${fmt(msg, arg1, arg2)}")}
-  override def debug(msg: String, arguments: AnyRef*): Unit         = if(isDebugEnabled){underlying.debug(s"$name ${fmt(msg, arguments)}")}
-  override def debug(msg: String, t: Throwable): Unit               = if(isDebugEnabled){underlying.debug(s"$msg: ${t.toString}")}
+  override def debug(msg: String): Unit                             = underlying.log(adjustLevel(Debug), s"$name $msg")
+  override def debug(msg: String, arg: AnyRef): Unit                = underlying.log(adjustLevel(Debug), s"$name ${fmt(msg, arg)}")
+  override def debug(msg: String, arg1: AnyRef, arg2: AnyRef): Unit = underlying.log(adjustLevel(Debug), s"$name ${fmt(msg, arg1, arg2)}")
+  override def debug(msg: String, arguments: AnyRef*): Unit         = underlying.log(adjustLevel(Debug), s"$name ${fmt(msg, arguments)}")
+  override def debug(msg: String, t: Throwable): Unit               = underlying.log(adjustLevel(Debug), s"$msg: ${t.toString}")
 
-  override def isInfoEnabled: Boolean                 = atLevel(Info)
-  override def isInfoEnabled(marker: Marker): Boolean = atLevel(Info)
+  override def isInfoEnabled: Boolean                 = underlying.atLevel(Info)
+  override def isInfoEnabled(marker: Marker): Boolean = underlying.atLevel(Info)
 
-  override def info(msg: String): Unit                             = if(isInfoEnabled){underlying.info(s"$name $msg")}
-  override def info(msg: String, arg: AnyRef): Unit                = if(isInfoEnabled){underlying.info(s"$name ${fmt(msg, arg)}")}
-  override def info(msg: String, arg1: AnyRef, arg2: AnyRef): Unit = if(isInfoEnabled){underlying.info(s"$name ${fmt(msg, arg1, arg2)}")}
-  override def info(msg: String, arguments: AnyRef*): Unit         = if(isInfoEnabled){underlying.info(s"$name ${fmt(msg, arguments)}")}
-  override def info(msg: String, t: Throwable): Unit               = if(isInfoEnabled){underlying.info(s"$msg: ${t.toString}")}
+  override def info(msg: String): Unit                             = underlying.log(adjustLevel(Info), s"$name $msg")
+  override def info(msg: String, arg: AnyRef): Unit                = underlying.log(adjustLevel(Info), s"$name ${fmt(msg, arg)}")
+  override def info(msg: String, arg1: AnyRef, arg2: AnyRef): Unit = underlying.log(adjustLevel(Info), s"$name ${fmt(msg, arg1, arg2)}")
+  override def info(msg: String, arguments: AnyRef*): Unit         = underlying.log(adjustLevel(Info), s"$name ${fmt(msg, arguments)}")
+  override def info(msg: String, t: Throwable): Unit               = underlying.log(adjustLevel(Info), s"$msg: ${t.toString}")
 
-  override def isWarnEnabled: Boolean                 = atLevel(Warn)
-  override def isWarnEnabled(marker: Marker): Boolean = atLevel(Warn)
+  override def isWarnEnabled: Boolean                 = underlying.atLevel(Warn)
+  override def isWarnEnabled(marker: Marker): Boolean = underlying.atLevel(Warn)
 
-  override def warn(msg: String): Unit                             = if(isWarnEnabled){underlying.warn(s"$name $msg")}
-  override def warn(msg: String, arg: AnyRef): Unit                = if(isWarnEnabled){underlying.warn(s"$name ${fmt(msg, arg)}")}
-  override def warn(msg: String, arg1: AnyRef, arg2: AnyRef): Unit = if(isWarnEnabled){underlying.warn(s"$name ${fmt(msg, arg1, arg2)}")}
-  override def warn(msg: String, arguments: AnyRef*): Unit         = if(isWarnEnabled){underlying.warn(s"$name ${fmt(msg, arguments)}")}
-  override def warn(msg: String, t: Throwable): Unit               = if(isWarnEnabled){underlying.warn(s"$msg: ${t.toString}")}
+  override def warn(msg: String): Unit                             = underlying.log(adjustLevel(Warn), s"$name $msg")
+  override def warn(msg: String, arg: AnyRef): Unit                = underlying.log(adjustLevel(Warn), s"$name ${fmt(msg, arg)}")
+  override def warn(msg: String, arg1: AnyRef, arg2: AnyRef): Unit = underlying.log(adjustLevel(Warn), s"$name ${fmt(msg, arg1, arg2)}")
+  override def warn(msg: String, arguments: AnyRef*): Unit         = underlying.log(adjustLevel(Warn), s"$name ${fmt(msg, arguments)}")
+  override def warn(msg: String, t: Throwable): Unit               = underlying.log(adjustLevel(Warn), s"$msg: ${t.toString}")
 
-  override def isErrorEnabled: Boolean                 = atLevel(Error)
-  override def isErrorEnabled(marker: Marker): Boolean = atLevel(Error)
+  override def isErrorEnabled: Boolean                 = underlying.atLevel(Error)
+  override def isErrorEnabled(marker: Marker): Boolean = underlying.atLevel(Error)
 
-  override def error(msg: String): Unit                             = if(isErrorEnabled){underlying.error(s"$name $msg")}
-  override def error(msg: String, arg: AnyRef): Unit                = if(isErrorEnabled){underlying.error(s"$name ${fmt(msg, arg)}")}
-  override def error(msg: String, arg1: AnyRef, arg2: AnyRef): Unit = if(isErrorEnabled){underlying.error(s"$name ${fmt(msg, arg1, arg2)}")}
-  override def error(msg: String, arguments: AnyRef*): Unit         = if(isErrorEnabled){underlying.error(s"$name ${fmt(msg, arguments)}")}
-  override def error(msg: String, t: Throwable): Unit               = if(isErrorEnabled){underlying.error(s"$msg: ${t.toString}")}
+  override def error(msg: String): Unit                             = underlying.log(adjustLevel(Error), s"$name $msg")
+  override def error(msg: String, arg: AnyRef): Unit                = underlying.log(adjustLevel(Error), s"$name ${fmt(msg, arg)}")
+  override def error(msg: String, arg1: AnyRef, arg2: AnyRef): Unit = underlying.log(adjustLevel(Error), s"$name ${fmt(msg, arg1, arg2)}")
+  override def error(msg: String, arguments: AnyRef*): Unit         = underlying.log(adjustLevel(Error), s"$name ${fmt(msg, arguments)}")
+  override def error(msg: String, t: Throwable): Unit               = underlying.log(adjustLevel(Error), s"$msg: ${t.toString}")
 }
